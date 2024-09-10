fb7ccc3aeb991e753815a7efebce78298e130bdd
Kauany Larissa <portifoliokauany@gmail.com>
2024-09-10 09:16:27 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,16 @@
+# Variáveis em python
+# não há necessidade de declará-las no inicio do programa
+# Basta criar no meio do algoritmo com a atribuição
+
+teste = "Eloyse"
+print(teste) # Imprime a string "Kauany"
+print(type(teste)) # imprime o tipo da variável, string
+teste = 10
+print(teste) # imprime o número 10
+print(type(teste)) # imprime o tipo da variável, que é int
+teste = 1.74
+print(teste) # imprime o númere 1.74
+print(type(teste)) # imprime o tipo da variável, que é float
+teste = True
+print(teste) #imprime o valor true
+print(type(teste)) # imprime o tipo da variável, que é bool
