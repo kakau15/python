87b6e244447829cdf9bf825a9f4d3dbbe6269e4f
Kauany Larissa <portifoliokauany@gmail.com>
2024-09-12 11:13:27 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,22 @@
+# Operadores aritméticos
+# Soma
+soma = 10 + 5
+print(soma)
+# Subtração
+subtracao = 10 - 5
+print(subtracao)
+# Multiplicação
+multiplicacao = 10 * 5
+print(subtracao)
+# Divisão
+divisao = 10 / 5
+print(divisao)
+# Divisão inteira
+Divisao_inteiro = 10 // 5
+print(Divisao_inteiro)
+# Exponenciação
+exponenciacao = 10 ** 5
+print(exponenciacao)
+# Módulo / Resto da divisão
+modulo = 10 % 5 
+print(modulo)
